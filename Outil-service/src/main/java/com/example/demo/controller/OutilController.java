721a6e0707f5906ff2d6df72ad523194960edb87
moncef <moncef.boukhris@enis.tn>
2022-11-28 13:10:33 +0100

    outil micro-service

--- Outil-service/src/main/java/com/example/demo/controller/OutilController.java
+++ Outil-service/src/main/java/com/example/demo/controller/OutilController.java
@@ -4,8 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.Outil;
@@ -22,5 +29,44 @@ public class OutilController {
 	public List<Outil> findOutils(){
 		return outilService.findAll();
 	}
+	@GetMapping(value="/outil/{id}")
+	public Outil findOneOutilById(@PathVariable Long id) {
+		return outilService.findOutil(id);
+	}
+	
+	@GetMapping(value="/outil/search/source")
+	public List<Outil> findOneOutilBySource(@RequestParam String source)
+	{
+		return outilService.findBySource(source);
+	}
+	
+	@PostMapping(value="outils/outil")
+	public Outil addOutil(@RequestBody Outil o) {
+		return outilService.addOutil(o);
+	}
+	
+	@DeleteMapping(value = "/outils/{id}")
+	public void deleteOutil(@PathVariable Long id)
+	{
+		outilService.deleteOutil(id);
+	}
+	
+	@PutMapping(value = "/outils/outil/{id}")
+	public Outil updateoutil(@PathVariable Long id,@RequestBody Outil o) {
+		o.setId(id);
+		return outilService.updateOutil(o);
+	}
+	
+	
 
 }
+
+
+
+
+
+
+
+
+
+
