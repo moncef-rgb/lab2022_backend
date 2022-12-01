3a75a02166115e7813f52a5a2a2bdb9560576fee
moncef <moncef.boukhris@enis.tn>
2022-12-01 14:21:11 +0100

    publication-service

---
+++ Publication-service/src/main/java/com/example/demo/PublicationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicationServiceApplication.class, args);
+	}
+
+}
